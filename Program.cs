4a55116d4294d5fba7b57b090a787cda98ff2fee
danielboros78 <danielboros78@gmail.com>
2021-09-14 08:56:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyzok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
